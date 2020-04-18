c46d904852747d91a189eafd1247469ebf2c13a9
Floris <flm.veldhuizen@gmail.com>
2020-04-19 01:43:27 +0200

    webcam is working
    if run too long, memory error occurs :(
    how to use sketch:
    move around with your mouse to draw with your screen as cursor
    press "p" to generate random background picture
    press "m" to change blending modes
    press "r" to reset effect
    press "c" to toggle cam mode

--- experiments/p5_frames/js/main.js
+++ experiments/p5_frames/js/main.js
@@ -8,33 +8,58 @@ function preload(){
 }
 
 function setup() {
+  imageMode(CENTER);
 	createCanvas(windowWidth, windowHeight);
-  image(img, 0, 0, width);
+  image(img, width/2, height/2, width);
+  noStroke();
+  fill(255);
   capture = createCapture(VIDEO);
   capture.size(width, height);
-  blendmodes = [BLEND, DARKEST, LIGHTEST, DIFFERENCE, MULTIPLY, EXCLUSION, SCREEN, OVERLAY, HARD_LIGHT, SOFT_LIGHT, DODGE, BURN, ADD];
+  blendmodes = [BLEND, DARKEST, LIGHTEST, DIFFERENCE, OVERLAY, HARD_LIGHT, SOFT_LIGHT, DODGE, BURN, ADD];
   i = 0;
 }
 
 function draw(){
   blendMode(blendmodes[i]);
-  if(webcamMode)
-    image(capture, 0, 0, width, height);
+  if(webcamMode) {
+    drawMask();
+    image(capture, width/2, height/2, width, height);
+  }
   c = get();
   drawImage();
+}
+
+function drawMask(){
+  // create mask
+  const maskImage = createGraphics(width,height);
+  maskImage.rect(0,0,width, height);
+  maskImage.erase();
+  maskImage.rect(mouseX-(width/4),mouseY-(height/4),width/frameSize, height/frameSize);
+  maskImage.noErase();
+  capture.mask(maskImage);
+  // //invert pixels
+  // const imgInverted = createGraphics(width,height);
+  // imgInverted.loadPixels();
+  // maskImage.loadPixels();
+  // for (var i = 3; i < imgInverted.pixels.length; i+=4) {
+  //   imgInverted.pixels[i] = 255-maskImage.pixels[i];
+  // }
+  // imgInverted.updatePixels();
+  // //set mask
+  // capture.mask(imgInverted);
 }
 
 function drawImage(){
   c.resize(width/frameSize, height/frameSize);
   c.set();
   c.updatePixels();
-  image(c, mouseX-width/(frameSize*2), mouseY-height/(frameSize*2));
+  image(c, mouseX, mouseY);
 }
 
 function keyTyped() {
   if (key==='m') {
     i++;
-    if(i>blendmodes.size-1) {
+    if(i>blendmodes.length-1) {
       i = 0;
     }
     console.log("BLENDMODE CHANGE! " + blendmodes[i]);
@@ -42,19 +67,23 @@ function keyTyped() {
     print('REFRESH!');
     blendMode(blendmodes[0]);
     if(webcamMode) {
-      image(capture, 0, 0, width, height);
+      image(capture, width/2, height/2, width, height);
     } else {
-      image(img, 0, 0, width);
+      image(img, width/2, height/2, width);
     }
   } else if (key === "p") {
     console.log("CHANGE MY PICTURE");
     img = loadImage('https://picsum.photos/' + width + '/' + height, _img => {
       blendMode(blendmodes[0]);
-      image(_img, 0, 0, width);
+      image(_img, width/2, height/2, width);
     });
   } else if (key === "c") {
     console.log("WEBCAM MODE ACTIVATE");
     webcamMode = !webcamMode;
     // image(capture, 0, 0, width);
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
